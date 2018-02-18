c412aa460e0fad225e413976006447aac65d52c8
ilya <ilya@localhost.localdomain>
2018-02-18 08:01:15 -0500

    install script for debian

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+#meant for debian-derived distros
+
+sudo apt-get install perl-doc lighttpd liburi-encode-perl libany-uri-escape-perl liburi-escape-perl libhtml-parser-perl
+git clone https://www.github.com/gulkily/duct
